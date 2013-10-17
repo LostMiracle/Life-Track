36dc38029cbc39437f544ff1975372c3de4bee12
LostMiracle <lostmiracle.me@gmail.com>
2013-10-16 22:40:50 -0700

    Content
    Added images and a poison counter.

--- LifeTrack/gen/com/lostmiracle/lifetrack/R.java
+++ LifeTrack/gen/com/lostmiracle/lifetrack/R.java
@@ -21,18 +21,25 @@ public final class R {
         public static final int activity_vertical_margin=0x7f050001;
     }
     public static final class drawable {
-        public static final int ic_launcher=0x7f020000;
-        public static final int splash=0x7f020001;
-        public static final int splash_background=0x7f020002;
+        public static final int heart=0x7f020000;
+        public static final int ic_launcher=0x7f020001;
+        public static final int poison=0x7f020002;
+        public static final int splash=0x7f020003;
+        public static final int splash_background=0x7f020004;
     }
     public static final class id {
-        public static final int action_settings=0x7f090006;
-        public static final int add_five=0x7f090003;
-        public static final int add_one=0x7f090002;
+        public static final int action_settings=0x7f09000b;
+        public static final int add_five=0x7f090008;
+        public static final int add_one=0x7f090004;
         public static final int bReset=0x7f090000;
-        public static final int minus_five=0x7f090004;
-        public static final int minus_one=0x7f090005;
-        public static final int tvCounter=0x7f090001;
+        public static final int imgHeart=0x7f09000a;
+        public static final int imgPoison=0x7f090009;
+        public static final int minus_five=0x7f090005;
+        public static final int minus_one=0x7f090006;
+        public static final int padd1=0x7f090001;
+        public static final int psub1=0x7f090003;
+        public static final int tvCounter=0x7f090007;
+        public static final int tvPoison=0x7f090002;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
--- LifeTrack/src/com/lostmiracle/lifetrack/MainActivity.java
+++ LifeTrack/src/com/lostmiracle/lifetrack/MainActivity.java
@@ -8,30 +8,43 @@ import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends Activity {
-	
-	int counter;
-	Button add1, add5, sub1, sub5, bReset;
-	TextView display;
+
+	int counter, pcounter;
+	Button add1, add5, sub1, sub5, padd1, psub1, bReset;
+	TextView display, pdisplay;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-		
+
+		pcounter = 0;
 		counter = 20;
+		padd1 = (Button) findViewById(R.id.padd1);
+		psub1 = (Button) findViewById(R.id.psub1);
 		add1 = (Button) findViewById(R.id.add_one);
 		add5 = (Button) findViewById(R.id.add_five);
 		sub1 = (Button) findViewById(R.id.minus_one);
 		sub5 = (Button) findViewById(R.id.minus_five);
 		bReset = (Button) findViewById(R.id.bReset);
 		display = (TextView) findViewById(R.id.tvCounter);
-		
+		pdisplay = (TextView) findViewById(R.id.tvPoison);
+
+		pdisplay.bringToFront();
+		add1.bringToFront();
+		add5.bringToFront();
+		sub1.bringToFront();
+		sub5.bringToFront();
+		display.bringToFront();
+
 		bReset.setOnClickListener(new View.OnClickListener() {
 
 			@Override
 			public void onClick(View v) {
 				// TODO Auto-generated method stub
 				counter = 20;
+				pcounter = 0;
+				pdisplay.setText("" + 0);
 				display.setText("" + 20);
 			}
 		});
@@ -69,6 +82,24 @@ public class MainActivity extends Activity {
 				// TODO Auto-generated method stub
 				counter -= 5;
 				display.setText("" + counter);
+			}
+		});
+		padd1.setOnClickListener(new View.OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				pcounter++;
+				pdisplay.setText("" + pcounter);
+			}
+		});
+		psub1.setOnClickListener(new View.OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				pcounter--;
+				pdisplay.setText("" + pcounter);
 			}
 		});
 	}
